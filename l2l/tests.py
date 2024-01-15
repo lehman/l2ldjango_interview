d3f62305534f659439e710719c737eb0b9fc755c
Louise Lehman <luhlahlehman@gmail.com>
2024-01-15 15:19:46 -0700

    test: add tests for toDateAndTimeString filter

--- l2l/tests.py
+++ l2l/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from l2l.templatetags.l2l_filters import toDateAndTimeString
+from datetime import datetime
 
-# Create your tests here.
+class toDateAndTimeStringTestCase(TestCase):
+    def test_datetime_param(self):
+        """when given datetime object, returns string of correct format"""
+        param = datetime(2024, 8, 30, 1, 2, 3)
+        # result = now|toDateAndTimeString
+        result = toDateAndTimeString(param)
+        self.assertEqual(result, '2024-08-30 01:02:03')
+
+    def test_string_param(self):
+        """when given string, returns string of correct format"""
+        param = "2024-01-15T21:41:28"
+        # result = param|toDateAndTimeString
+        result = toDateAndTimeString(param)
+        self.assertEqual(result, '2024-01-15 21:41:28')
